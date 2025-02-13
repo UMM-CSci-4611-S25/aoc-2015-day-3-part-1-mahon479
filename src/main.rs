cf2557a059eaf961cde663b65d7de169d7a603ff
mahon479 <mahon479@morris.umn.edu>
2025-02-13 16:12:51 -0600

    Updated things in class. The test_move_east test is passing.

--- src/main.rs
+++ src/main.rs
@@ -6,10 +6,24 @@ fn main() {
     println!("{}", contents.len());
 }
 
-#[derive(PartialEq, Eq, Debug)]
+#[derive(PartialEq, Eq, Debug, Copy, Clone, Hash)]
 pub struct Pos {
     x: i32,
     y: i32,
+}
+
+impl Pos {
+    #[must_use]
+    pub const fn new(x: i32, y: i32) -> Self {
+        Self {x, y}
+    }
+}
+
+pub enum Direction {
+    North,
+    South,
+    East,
+    West,
 }
 pub struct VisitedHouses {
     visited_houses: HashSet<Pos>,
@@ -17,19 +31,37 @@ pub struct VisitedHouses {
 }
 
 impl VisitedHouses {
-    pub fn new() -> VisitedHouses {
-        VisitedHouses{
-            visited_houses: todo!(),
-            current_position: Pos{x: 0, y: 0},
+    #[must_use]
+    pub fn new() -> Self {
+        let initial_pos = Pos::new(0, 0);
+        let mut visited_houses = HashSet::new();
+        visited_houses.insert(initial_pos);
+
+        Self {
+            visited_houses,
+            current_position: initial_pos,
         }
     }
 
-    pub fn num_visited_houses(&self) -> i32 {
-        1
+    #[must_use]
+    pub fn num_visited_houses(&self) -> usize {
+        self.visited_houses.len()
     }
 
-    pub fn current_pos(&self) -> Pos {
+    #[must_use]
+    pub const fn current_pos(&self) -> Pos {
         self.current_position
+    }
+
+    pub fn perform_move(&mut self, direction: Direction){
+        let new_position: Pos = match direction {
+            Direction::North => Pos::new(self.current_position.x, self.current_position.y + 1),
+            Direction::South => Pos::new(self.current_position.x, self.current_position.y - 1),
+            Direction::East => Pos::new(self.current_position.x + 1, self.current_position.y),
+            Direction::West => Pos::new(self.current_position.x - 1, self.current_position.y),
+        };
+        self.current_position = new_position;
+        self.visited_houses.insert(new_position);
     }
 }
 
@@ -61,13 +93,13 @@ mod tests {
     //     assert_eq!('x'.try_into(), Err(IllegalDirectionCharacter('x')));
     // }
 
-    // #[test]
-    // fn test_move_east() {
-    //     let mut visited_houses = VisitedHouses::new();
-    //     visited_houses.perform_move(Direction::East);
-    //     assert_eq!(visited_houses.num_visited_houses(), 2);
-    //     assert_eq!(visited_houses.current_pos(), Pos(1, 0));
-    // }
+    #[test]
+    fn test_move_east() {
+        let mut visited_houses = VisitedHouses::new();
+        visited_houses.perform_move(Direction::East);
+        assert_eq!(visited_houses.num_visited_houses(), 2);
+        assert_eq!(visited_houses.current_pos(), Pos::new(1, 0));
+    }
 
     // #[test]
     // fn test_square_moves() {
@@ -75,7 +107,7 @@ mod tests {
     //     let moves = Moves::from_str("^>v<").unwrap();
     //     visited_houses.perform_moves(moves);
     //     assert_eq!(visited_houses.num_visited_houses(), 4);
-    //     assert_eq!(visited_houses.current_pos(), Pos(0, 0));
+    //     assert_eq!(visited_houses.current_pos(), Pos::new(0, 0));
     // }
 
     // #[test]
@@ -84,7 +116,7 @@ mod tests {
     //     let moves = Moves::from_str("^v^v^v^v^v").unwrap();
     //     visited_houses.perform_moves(moves);
     //     assert_eq!(visited_houses.num_visited_houses(), 2);
-    //     assert_eq!(visited_houses.current_pos(), Pos(0, 0));
+    //     assert_eq!(visited_houses.current_pos(), Pos::new(0, 0));
     // }
 
     // #[test]
